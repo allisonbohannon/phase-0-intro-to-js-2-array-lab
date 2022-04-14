3261ad7b16642e163d9731c336db59f6a6e806d3
Allison Bohannon <akboh.github@gmail.com>
2022-04-14 12:07:56 -0700

    commit tree f9136a270b06383ceecfb5b8905ba45e168eaa73

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]; 
+
+function destructivelyAppendCat(newCat) {
+    cats.push(newCat); 
+    return cats; 
+}; 
+
+function destructivelyPrependCat(newCat) {
+    cats.unshift(newCat); 
+    return cats; 
+}; 
+
+function destructivelyRemoveFirstCat() {
+    cats.shift(); 
+    return cats; 
+
+}; 
+
+function destructivelyRemoveLastCat() {
+    cats.pop(); 
+    return cats; 
+}
+
+
+function appendCat(newCat) {
+    const newArr = [...cats, newCat]; 
+    return newArr; 
+}; 
+
+function prependCat(newCat) {
+    const newArr = [newCat, ...cats]; 
+    return newArr; 
+}; 
+
+function removeFirstCat() {
+    return cats.slice(1); 
+}; 
+
+function removeLastCat() {
+    return cats.slice(0, cats.length - 1)
+};
